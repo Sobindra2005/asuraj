8af045e9273cb397bbc0ea7efcf4462a4499a10b
Sobindra2005 <sobhindra2005@gmail.com>
2024-05-14 20:10:13 +0545

    small

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const express=require("express");
 const app=express()
 const port=3000;
-const{connectToMongoDB}=require('./authentication/mongodbConnection')
+const{connectToMongoDB}=require('./mongodbConnection')
 const loginRoutes =require('./authentication/routes/login')
 const signupRoutes=require('./authentication/routes/signup')
 const homeRouter=require('./authentication/routes/home')
 const{tokenAuthenticate}=require('./authentication/middleware/authenticationCheck')
 
-app.use(express.urlencoded({extended:false})) //body parser
+app.use(express.urlencoded({extended:false})) 
 
-//connection to the mongodb 
+
 require('dotenv').config()
 const uri=process.env.uri
 connectToMongoDB(uri);
 
-//routes
+
 app.use('/',loginRoutes)
 app.use('/',signupRoutes)
 app.use('/home/',tokenAuthenticate,homeRouter)
